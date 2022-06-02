3d6f6004a2e45197f87b7df926afd1d9b78b8776
hanifur-rahman-ridoy <hrridoy8643@gmail.com>
2022-06-02 19:53:59 +0600

    new update

---
+++ PHP/BASIC/0) flot_to_int.php
@@ -0,0 +1,26 @@
+<?php
+
+$d = 0;
+$c = 2.34;
+$a = 3.67325;
+
+echo (round($d)); // round means Round Figure
+echo "<br/>";
+echo ((int)($d)); //Int means . er ager purno number only
+
+echo "<br/>";
+
+$outputFirst = ((int)($c)); 
+$outputRoundFg = (round($a));
+
+echo $outputFirst;
+echo "<br/>";
+echo $outputRoundFg;
+
+//   NOTE : is_int means eta ki integer typer data? true and false return korve .
+// $a = "hellow";
+// $result = (is_int($a));
+// var_dump($result);
+//========================================
+
+
---
+++ PHP/BASIC/10 variable scope.php
@@ -0,0 +1,41 @@
+<?php
+// ===========Global Scope=================
+// $x = 10;
+// printValue();
+
+// function printValue()
+// {
+//     global $x;
+//     echo "X=" . $x;
+//     echo "<br/>";
+
+// }
+
+// ======================Global Scope=============
+//global deuar karone akta global variable diclar hoise
+
+// printValue1();
+// echo $x1;
+// echo "<br/>";
+// function printValue1()
+// {
+//     global $x1;
+//     $x1 = 5;
+
+// }
+
+// ==============Static Scope===================
+//last Variavle er value niya kaj kore
+
+// countNum();
+// countNum();
+// countNum();
+// function countNum()
+// {
+//     static $x = 0;
+//     $x++;
+//     echo $x . "<br/>";
+
+// }
+
+
